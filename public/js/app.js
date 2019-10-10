e414420e8c9d3b9c080666160c9c57424a2d4ad3
Ahmed Yousef <ahmedyosuefarnous@gmail.com>
2019-10-10 03:35:38 +0200

    Fix CSP Bug

--- public/js/app.js
+++ public/js/app.js
@@ -47352,8 +47352,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Projects\StudentMate\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Projects\StudentMate\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! E:\Projects\StudentMate\Backend\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! E:\Projects\StudentMate\Backend\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
